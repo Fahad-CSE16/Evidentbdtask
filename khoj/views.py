6b2b95f160ad74de70f0664e96aa164ff0609738
Fahad-CSE16 <mdfahadhossain>
2021-09-21 20:57:26 +0600

    project working in basic way

--- khoj/views.py
+++ khoj/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
 from .models import *
+import re
 # Create your views here.
 def khoj_the_search(request):
     if request.method=="POST":
         arr=request.POST['array']
         query=request.POST['query']
-        arr = arr.split(",")
+
+        arr=re.split(',|;|, |,,|,',arr)
+        
+        print(arr)
+
+        for i in arr:
+            if i is None or i =='':
+                arr.remove(i)
+                print("call")
+        arr = [int(i) for i in arr]
         arr=sorted(arr,reverse=True)
-        if query in arr:
-            result=True
+
+        if int(query) in arr:
+            result="True"
         else:
-            result=False
+            result="False"
+        print(arr)
+
         Results.objects.create(user=request.user,result=result, search_value= query,input_values=arr )
         return render(request,'khoj/khoj.html',{'result':result})
     return render(request,'khoj/khoj.html')
