6fae2b280af4892fdb60343742299d75d57ba787
Fahad-CSE16 <mdfahadhossain>
2021-09-23 22:56:59 +0600

    code check done

--- evidentbd/urls.py
+++ evidentbd/urls.py
@@ -3,17 +3,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from rest_framework.authtoken import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('userapp.urls')),
     path('khoj/', include('khoj.urls')),
+
     path('api/', include('rest_framework.urls')),
+
+    #  API for Getting Token for Authentication
     path('api-token-auth/', views.obtain_auth_token),
+
+    # Api for JWT Token Authentication
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- userapp/views.py
+++ userapp/views.py
@@ -1,25 +1,13 @@
-from django.shortcuts import render
-from django.db.models import Q
-#basic import
-from django.shortcuts import render, HttpResponse,redirect,HttpResponseRedirect
-from django.http import HttpResponseRedirect,HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.views.generic import View
-import time
-#Userlogin signup, import
-from django.contrib.auth import authenticate, login, logout, update_session_auth_hash,get_user_model
-from django.contrib.auth.views import PasswordChangeForm, PasswordResetCompleteView,\
-     PasswordResetConfirmView, PasswordResetView,PasswordResetForm,PasswordResetDoneView
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib.auth.decorators import login_required
-#LocAL IMPORT
+from django.views.generic import TemplateView
+
 from .models import *
 from .forms import *
 from .decorators import unauthenticated_user
 User = get_user_model()
-from django.views.generic import TemplateView
-
-
 class HomeView(TemplateView):
     template_name='home.html'
 
@@ -27,11 +15,8 @@ class HomeView(TemplateView):
 def handleSignup(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
-        # print(form.errors.as_data())
         if form.is_valid():
-            print("calling")
             user = form.save()
-            print("user", user)
             messages.info(request, 'Successfully Registered!')
             login(request, user)
             messages.success(request, f"You are now logged in as {user.email}")
@@ -45,18 +30,15 @@ def handleLogin(request):
     if request.method == 'POST':
         email =request.POST['email']
         password = request.POST['password']
-        print(email, password)
         user = authenticate(username=email, password=password)
-        print("user", user)
         if user is not None:
             login(request, user)
             messages.success(request, f"You are now logged in as {user.email}")
             return redirect('home')
         else:
             messages.error(request, "Invalid email or password.")
+    return render(request=request, template_name="userapp/login.html")
 
-    return render(request=request,
-                  template_name="userapp/login.html",)
 @login_required(login_url='login')
 def handleLogout(request):
     logout(request)
